53b39f3baf9eb7bbedb63bb5ce923c73f1288312
현재 <phj2394324@naver.com>
2025-09-25 18:49:26 +0900

    [boj] Matrix multiplication #1

--- java/src/study/algorithm/Matrix_Multiplication.java
+++ java/src/study/algorithm/Matrix_Multiplication.java
@@ -1,6 +1,4 @@
-package test.test09.test0925;
-
-import java.io.IOException;
+package study.algorithm;
 
 /*
 
@@ -23,52 +21,49 @@ import java.io.IOException;
 import java.io.*;
 import java.util.*;
 
-public class Main2740 {
+public class Matrix_Multiplication {
     
-    static int N1 = 0;
-    static int K2 = 0;
-    static int[][] result;
     static StringBuilder sb = new StringBuilder();
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N1 = Integer.parseInt(st.nextToken());
-        int M1 = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        int[][] arr1 = new int[N1][M1];
-        for (int i = 0; i < N1; i++) {
-            StringTokenizer st1 = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M1; j++) {
-                arr1[i][j] = Integer.parseInt(st1.nextToken());
+        int[][] arr1 = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                arr1[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
-        int M2 = Integer.parseInt(st2.nextToken());
-        K2 = Integer.parseInt(st2.nextToken());
+        st = new StringTokenizer(br.readLine());
 
-        int[][] arr2 = new int[M2][K2];
+        int M2 = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        
+        int[][] arr2 = new int[M2][K];
         for (int i = 0; i < M2; i++) {
-            StringTokenizer st3 = new StringTokenizer(br.readLine());
-            for (int j = 0; j < K2; j++) {
-                arr2[i][j] = Integer.parseInt(st3.nextToken());
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < K; j++) {
+                arr2[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
         multiply(arr1, arr2);
-
+        
         System.out.println(sb);
         br.close();
     }
 
     static void multiply(int[][] A, int[][] B) {
-        result = new int[N1][K2];
-        int A_row = A.length;  
+        int A_row = A.length;
         int A_col = A[0].length;
         int B_col = B[0].length;
 
+        int[][] result = new int[A_row][B_col];
         for (int i = 0; i < A_row; i++) {
             for (int j = 0; j < B_col; j++) {
                 for (int k = 0; k < A_col; k++) {
