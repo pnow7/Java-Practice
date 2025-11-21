017ca1f0a602f198a1345e97a5b0fd8d7b6edea0
현재 <phyunjae7333@gmail.com>
2025-11-22 00:54:11 +0900

    [boj] DFS 2(내림차순 방문) #1

---
+++ java/src/test/test11/test1121/Main24480.java
@@ -0,0 +1,84 @@
+package test.test11.test1121;
+
+/*
+- 깊이 우선 탐색 2
+
+정점번호를 내림차순
+
+[입력]
+5 5 1
+1 4
+1 2
+2 3
+2 4
+3 4
+
+[출력]
+1
+4
+3
+2
+0
+
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Main24480 {
+	
+	static ArrayList<Integer>[] graph;
+	static int[] order;
+	static int count = 1;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int R = Integer.parseInt(st.nextToken());
+		
+		order = new int[N + 1];
+		graph = new ArrayList[N + 1];
+		
+		for (int i = 1; i <= N; i++) {
+			graph[i] = new ArrayList<>();
+		}
+		
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			
+			graph[u].add(v);
+			graph[v].add(u);
+		}
+		
+		for (int i = 1; i <= N; i++) {
+			Collections.sort(graph[i], Collections.reverseOrder());
+		}
+		
+		DFS(R);
+		
+		for (int i = 1; i <= N; i++) {
+			sb.append(order[i]).append("\n");
+		}
+		
+		System.out.println(sb.toString());
+		br.close();
+	}
+	
+	static void DFS(int startNode) {
+		order[startNode] = count;
+		
+		for (int nextNode : graph[startNode]) {
+			if (order[nextNode] == 0) {
+				count++;
+				DFS(nextNode);
+			}
+		}
+	}
+	
+}
