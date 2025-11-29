a349b85744955e48691e5fc3a6f6bf579dd90a49
현재 <phyunjae7333@gmail.com>
2025-11-29 13:53:30 +0900

    Fix: 중복 파일 교체

--- java/src/study/algorithm/bfs_dfs/dfs/DFS_LandCabbage.java
+++ java/src/study/algorithm/bfs_dfs/dfs/DFS_LandCabbage.java
@@ -1,36 +1,61 @@
 package study.algorithm.bfs_dfs.dfs;
 
 /*
-- 단지번호붙이기
+- 유기농 배추
 
-1은 집이 있는 곳
-0은 집이 없는 곳
+1	1	0	0	0	0	0	0	0	0
+0	1	0	0	0	0	0	0	0	0
+0	0	0	0	1	0	0	0	0	0
+0	0	0	0	1	0	0	0	0	0
+0	0	1	1	0	0	0	1	1	1
+0	0	0	0	1	0	0	1	1	1
 
-연결된 집의 모임인 단지를 정의
-단지에 번호를 붙이려 함
+0은 배추 X
+1은 배추 O
 
-연결되었다는 것은 어떤 집이 좌우, 
-혹은 아래위로 다른 집이 있는 경우를 말함.
+최소 5마리의 배추흰지렁이 필요
 
-대각선상에 집이 있는 경우는 연결된 것이 아님.
-지도를 입력하여 단지수를 출력하고,
-각 단지에 속하는 집의 수를 오름차순으로 정렬하여 출력
+입력: 
+첫 줄에는 테스트 케이스 개수 T
+
+첫째 줄에는 
+배추를 심은 배추밭의 가로길이 M(1 <= M <= 50), 
+세로길이 N(1 <= N <= 50),
+배추가 심어져 있는 위치의 개수 K(1 <= K <= 2500)
+
+그 다음 K줄
+배추의 위치 X(0 <= X <= M - 1),
+Y(0 <= Y <= N - 1)가 주어진다.
+
+두 배추의 위치가 같은 경우는 없음
 
 [입력]
-7
-0110100
-0110101
-1110101
-0000111
-0100000
-0111110
-0111000
+2
+10 8 17
+0 0
+1 0
+1 1
+4 2
+4 3
+4 5
+2 4
+3 4
+7 4
+8 4
+9 4
+7 5
+8 5
+9 5
+7 6
+8 6
+9 6
+10 10 1
+5 5
 
 [출력]
-3
-7
-8
-9
+5
+1
+
 */
 
 import java.io.*;
@@ -38,43 +63,42 @@ import java.util.*;
 
 public class DFS_LandCabbage {
 
+	static int[][] land;
 	static boolean[][] visited;
-	static int[][] house;
 	static int count;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		ArrayList<Integer> list = new ArrayList<>();
 		StringBuilder sb = new StringBuilder();
+		int t = Integer.parseInt(br.readLine());
 		
-		int n = Integer.parseInt(br.readLine());
-		
-		visited = new boolean[n][n];
-		house = new int[n][n];
-		
-		for (int i = 0; i < n; i++) {
-			String num = br.readLine();
-			for (int j = 0; j < n; j++) {
-				// house[i][j] = Integer.parseInt(String.valueOf(num.charAt(j));
-				house[i][j] = num.charAt(j) - '0';
+		while (t-- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int m = Integer.parseInt(st.nextToken());
+			int n = Integer.parseInt(st.nextToken());
+			int k = Integer.parseInt(st.nextToken());
+			
+			land = new int[m][n];
+			visited = new boolean[m][n];
+			
+			for (int i = 0; i < k; i++) {
+				st = new StringTokenizer(br.readLine());
+				int x = Integer.parseInt(st.nextToken());
+				int y = Integer.parseInt(st.nextToken());
+				land[x][y] = 1;
 			}
-		}
-		
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (house[i][j] == 1 && visited[i][j] == false) {
-					count = 0;
-					dfs(i, j);
-					list.add(count);
+			
+			count = 0;
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < n; j++) {
+					if (land[i][j] == 1 && visited[i][j] == false) {
+						dfs(i, j);
+						count++;
+					}
 				}
 			}
-		}
-		
-		Collections.sort(list);
-		
-		sb.append(list.size()).append("\n");
-		for (int result : list) {
-			sb.append(result).append("\n");
+			
+			sb.append(count).append("\n");
 		}
 		
 		System.out.println(sb.toString());
@@ -86,14 +110,13 @@ public class DFS_LandCabbage {
 		int[] dy = {0, 0, 1, -1};
 		
 		visited[x][y] = true;
-		count++;
 		
 		for (int i = 0; i < 4; i++) {
 			int nx = x + dx[i];
 			int ny = y + dy[i];
 			
-			if ((0 <= nx && nx < house.length) && (0 <= ny && ny < house.length)) { 
-				if (house[nx][ny] == 1 && visited[nx][ny] == false) {
+			if ((0 <= nx && nx < land.length) && (0 <= ny && ny < land[0].length)) {
+				if (visited[nx][ny] == false && land[nx][ny] == 1) {
 					visited[nx][ny] = true;
 					dfs(nx, ny);
 				}
