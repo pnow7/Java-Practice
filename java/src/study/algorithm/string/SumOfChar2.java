30c639c8a6ea14b0d083128ed3a70b8d0a92cb06
현재 <phyunjae7333@gmail.com>
2025-12-11 17:38:22 +0900

    [boj] String ↔ Char 연습 #1

---
+++ java/src/study/algorithm/string/SumOfChar2.java
@@ -0,0 +1,85 @@
+package study.algorithm.string;
+
+/*
+- 이 구역의 승자는 누구야?
+
+알파벳 대문자로만 이루어진 문자열이 주어진다. 
+문자열이 주어지면 각 문자의 획수로 문자를 변환한다. 
+획수들을 갖고 앞에서부터 두 개씩 더해가는데 만약 짝이 지어지지 않는다면 
+그대로 다음 단계로 내려간다. 
+
+다음 단계부터는 이전 단계에서 
+두 개씩 더해가며 생성된 숫자들을 가지고 같은 과정을 반복한다. 
+과정을 반복하다가 결국 마지막 한 개의 수가 남았을 때 
+그 수가 홀수라면 이기는 것이고 짝수라면 지는 게임이다
+
+예를 들어 "ABCDE"라는 문자열이 주어지면 
+‘A’, ‘B’, ‘C’, ‘D’, ‘E’ 각 문자의 획수인 
+3, 2, 1, 2, 3으로 바꾸어 아래의 그림처럼 과정을 진행한다. 
+
+단, 계산할 때, 더한 값이 10을 넘는다면 
+10으로 나눈 나머지로 바꿔준다.
+
+‘E’의 경우는 짝을 지을 수 없으므로 3이 바로 내려오게 된다. 
+결국, 마지막 남은 수가 1인 홀수이므로 이 게임은 이기게 되는 것이다.
+
+A	B	C	D	E	F	G	H	I	J	K	L	M	N	O	P	Q	R	S	T	U	V	W	X	Y	Z
+3	2	1	2	3	3	3	3	1	1   3	1	3	3	1   2	2	2	1	2   1	1	2	2	2	1
+
+[입력]
+ABCDE
+
+AECF
+
+[출력]
+I'm a winner!
+
+You're the winner?
+
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class SumOfChar2 {
+
+	private static final int[] ALPHABETVALUES = {
+			3, 2, 1, 2, 	3, 3	, 3, 3, 	1, 1,
+			3, 1, 3, 3, 	1, 2, 2, 2, 	1, 2,
+			1, 1, 2, 2, 2, 1
+	};
+	private static final String WINNER = "I'm a winner!";
+	private static final String LOSER = "You're the winner?";
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		List<Integer> list = new ArrayList<>();
+		
+		String word = br.readLine();
+		
+		for (char c : word.toCharArray()) {
+			list.add(ALPHABETVALUES[c - 'A']);
+		}
+		
+		while (list.size() > 1) {
+			List<Integer> nextList = new ArrayList<>();
+			
+			for (int i = 0; i < list.size() / 2; i++) {
+				int sum = list.get(2 * i) + list.get(2 * i + 1);
+				nextList.add(sum % 10);
+			}
+			
+			if (list.size() % 2 != 0) {
+				nextList.add(list.get(list.size() - 1));
+			}
+			
+			list = nextList;
+		}
+		
+		int result = list.get(0);
+	
+		System.out.println(result % 2 != 0 ? WINNER : LOSER);
+		br.close();
+	}
+
+}
---
+++ java/src/study/algorithm/string/VerticalReading.java
@@ -0,0 +1,79 @@
+package study.algorithm.string;
+
+/*
+- 세로읽기
+
+글자들은 영어 대문자 ‘A’부터 ‘Z’, 
+영어 소문자 ‘a’부터 ‘z’, 숫자 ‘0’부터 ‘9’이다. 
+영석이는 칠판에 글자들을 수평으로 일렬로 붙여서 단어를 만든다. 
+다시 그 아래쪽에 글자들을 붙여서 또 다른 단어를 만든다. 
+이런 식으로 다섯 개의 단어를 만든다. 
+ 
+A A B C D D
+a f z z 
+0 9 1 2 1
+a 8 E W g 6
+P 5 h 3 k x
+
+다섯 개의 단어를 세로로 읽으려 한다. 
+세로로 읽을 때, 각 단어의 첫 번째 글자들을 위에서 아래로 세로로 읽는다. 다음에 두 번째 글자들을 세로로 읽는다. 
+이런 식으로 왼쪽에서 오른쪽으로 한 자리씩 이동 하면서 
+동일한 자리의 글자들을 세로로 읽어 나간다.
+
+세로로 읽을 때 해당 자리의 글자가 없으면, 
+읽지 않고 그 다음 글자를 계속 읽는다. 
+그림 1의 다섯 번째 자리를 세로로 읽으면 D1gk로 읽는다.
+
+영석이가 세로로 읽은 순서대로 글자들을 공백 없이 출력하면 다음과 같다:
+Aa0aPAf985Bz1EhCz2W3D1gkD6x
+
+[입력]
+ABCDE
+abcde
+01234
+FGHIJ
+fghij
+
+AABCDD
+afzz
+09121
+a8EWg6
+P5h3kx
+
+[출력]
+Aa0FfBb1GgCc2HhDd3IiEe4Jj
+
+Aa0aPAf985Bz1EhCz2W3D1gkD6x
+
+*/
+
+import java.io.*;
+
+public class VerticalReading {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		char[][] ch = new char[5][15];
+		
+		for (int i = 0; i < 5; i++) {
+			String word = br.readLine();
+			for (int j = 0; j < word.length(); j++) {
+				ch[i][j] = word.charAt(j);
+			}
+		}
+		
+		for (int i = 0; i < 15; i++) {
+			for (int j = 0; j < 5; j++) {
+				if (ch[j][i] != 0) {
+					sb.append(ch[j][i]);
+				}
+			}
+		}
+		
+		System.out.println(sb.toString());
+		br.close();
+	}
+	
+}
