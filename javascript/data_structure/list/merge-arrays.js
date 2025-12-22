9e177bc43c048cf66cfd42c46c5cf418b52fab36
현재 <phyunjae7333@gmail.com>
2025-12-22 19:57:38 +0900

    [Update] remote 변경
    Signed-off-by: 현재 <phyunjae7333@gmail.com>

---
+++ javascript/data_structure/list/merge-arrays.js
@@ -0,0 +1,31 @@
+/*
+- 배열 합치기
+
+정렬되어있는 두 배열 A와 B가 주어진다.
+두 배열을 합친 다음 정렬해서 출력
+
+첫째 줄에 배열 A의 크기 N, 배열 B의 크기 M이 주어짐
+둘째 줄에는 배열 A의 내용,
+셋째 줄에는 배열 B의 내용이 주어짐.
+
+[입력]
+2 2
+3 5
+2 9
+
+2 1
+4 7
+1
+
+[출력]
+2 3 5 9
+
+1 4 7
+*/
+
+const fs = require('fs')
+const input = fs.readFileSync("./input.txt").trim().split(/\+s/);
+
+let idx = 0;
+const N = Number(input[idx++]);
+const M = Number(input[idx++]);
