528abf0ca2716c60d346f31fc85eb92fcbce21cc
현재 <phyunjae7333@gmail.com>
2025-12-22 18:19:25 +0900

    [Update] 자료구조로 패키지 분리

---
+++ java/src/study/datastructure/setmap/TreeMapOrganizeFiles.java
@@ -0,0 +1,83 @@
+package study.datastructure.setmap;
+
+/*
+- 파일 정리
+
+"바탕화면의 파일들에는 값진 보물에 대한 정보가 들어 있어. 
+하나라도 지우게 된다면 보물은 물론이고 다시는 노트북을 쓸 수 없게 될 거야. 
+파일들을 잘 분석해서 보물의 주인공이 될 수 있길 바랄게. 힌트는 “확장자”야."
+
+파일을 확장자 별로 정리해서 몇 개씩 있는지 알려줘
+보기 편하게 확장자들을 사전 순으로 정렬해 줘
+
+첫째 줄에 바탕화면에 있는 파일의 개수 
+N이 주어진다. (1 <= N <= 50000)
+
+둘째 줄부터 N개 줄에 바탕화면에 있는 파일의 이름이 주어진다. 
+파일의 이름은 알파벳 소문자와 점(.)으로만 구성되어 있다. 
+점은 정확히 한 번 등장하며, 파일 이름의 첫 글자 또는 마지막 글자로 오지 않는다.
+
+확장자가 여러 개 있는 경우 확장자 이름의 사전순으로 출력한다.
+
+[입력]
+8
+sbrus.txt
+spc.spc
+acm.icpc
+korea.icpc
+sample.txt
+hello.world
+sogang.spc
+example.txt
+
+[출력]
+icpc 2
+spc 2
+txt 3
+world 1
+
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class TreeMapOrganizeFiles {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		TreeMap<String, Integer> map = new TreeMap<>();
+		int N = Integer.parseInt(br.readLine());
+		
+		while (N-- >0) {
+			String[] fileName = br.readLine().split("\\.");
+			int fileType = fileName.length - 1;
+			
+			map.put(fileName[fileType], map.getOrDefault(fileName[fileType], 0) + 1);
+		}
+		
+		
+		for (String key : map.keySet()) {
+			Integer value = map.get(key);
+			
+			sb.append(key).append(" ").append(value).append("\n");
+		}
+		
+		System.out.println(sb.toString());
+		br.close();
+	}
+
+}
+
+/*
+
+=> TreeMap (Key 값을 기반으로 정렬할 필요 없이 자동으로 오름차순 적용) 
+
+[오름차순]
+TreeMap<String, Integer> map = new TreeMap<>();
+
+[내림차순]
+TreeMap<String, Integer> map = new TreeMap<>(Comparator.reverseOrder());
+
+*/
---
+++ java/src/study/datastructure/stack/Stack1.java
@@ -0,0 +1,31 @@
+package study.datastructure.stack;
+
+import java.util.*;
+public class Stack1 {
+	
+	public static void main(String[] args) {
+		
+        Stack<Integer> stack = new Stack<>();
+
+        // push(x): 요소 추가
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        System.out.println("Stack: " + stack); // [10, 20, 30]
+
+        // pop(): 맨 뒤 요소 뽑음(뽑으)
+        int top = stack.pop();
+        System.out.println("pop(): " + top); // 30
+        System.out.println("Stack after pop: " + stack); // [10, 20]
+
+        // peek(): 맨 뒤 요소 조회(조회)
+        System.out.println("peek(): " + stack.peek()); // 20
+
+        // isEmpty(): 스택이 비어 있는지 확인
+        System.out.println("isEmpty(): " + stack.isEmpty()); // false
+
+        // size(): 스택 크기 확인
+        System.out.println("size(): " + stack.size()); // 2
+    }
+}
